63c138713513159a48a98a822aa1d9349c06325d
Will <williamkerwin@lancsfirerescue.org.uk>
2024-06-19 20:06:53 +0100

    test rename

--- storage_test.go
+++ storage_test.go
@@ -10,13 +10,13 @@ import (
 func TestPathTransformFunc(t *testing.T) {
 	key := "momsbestpictures"
 	pathKey := CASPathTransformFunc(key)
-	expectedOriginalKey := "52439f5e49ba33b4b9b2373dcd6bfc9e97cafb7f"
+	expectedFileName := "52439f5e49ba33b4b9b2373dcd6bfc9e97cafb7f"
 	expectedPathname := "52439/f5e49/ba33b/4b9b2/373dc/d6bfc/9e97c/afb7f"
 	if pathKey.Pathname != expectedPathname {
 		t.Errorf("%s want %s\n", pathKey.Pathname, expectedPathname)
 	}
-	if pathKey.Filename != expectedOriginalKey {
-		t.Errorf("%s want %s\n", pathKey.Filename, expectedOriginalKey)
+	if pathKey.Filename != expectedFileName {
+		t.Errorf("%s want %s\n", pathKey.Filename, expectedFileName)
 	}
 }
 
